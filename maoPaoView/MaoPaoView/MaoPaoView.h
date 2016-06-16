6a4c5ee6aea72218cd47494cb2d00193d92d6607
apple <mac@macs-macbook-air.local>
2016-06-16 16:57:07 +0800

    新建MaoPao文件

---
+++ maoPaoView/MaoPaoView/MaoPaoView.h
@@ -0,0 +1,13 @@
+//
+//  MaoPaoView.h
+//  maoPaoView
+//
+//  Created by mac on 16/6/16.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MaoPaoView : UIView
+
+@end
---
+++ maoPaoView/MaoPaoView/MaoPaoView.m
@@ -0,0 +1,26 @@
+//
+//  MaoPaoView.m
+//  maoPaoView
+//
+//  Created by mac on 16/6/16.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import "MaoPaoView.h"
+
+@implementation MaoPaoView
+
+- (void)layoutSubviews{
+    [super layoutSubviews];
+    
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
